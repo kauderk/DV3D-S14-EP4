ad79b5b8b9fd12f4d78aba2fdf90ff0992eb6398
kauderk <65237382+kauderk@users.noreply.github.com>
2022-07-05 23:30:32 -0500

    Reimport unitypackage and moved asset structure to Package

---
+++ Packages/DV3D-S14-EP4/Runtime/Scripts/Input/MobileInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+public class MobileInput : IUserInput, ITickable
+{
+    public event Action OnPress = null;
+
+    public void Tick()
+    {
+        foreach (var touch in Input.touches)
+        {
+            if (touch.fingerId == 0 && touch.phase == TouchPhase.Began)
+                OnPress?.Invoke();
+        }
+    }
+}
---
+++ Packages/DV3D-S14-EP4/Runtime/Scripts/Views/TapView.cs
@@ -0,0 +1,44 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+public class TapView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _tapText = null;
+
+    private IUserInput _userInput = null;
+    private Player _player = null;
+    
+    private const string START_TEXT = "TAP TO PLAY!";
+    private const string RESTART_TEXT = "TAP TO RESTART!";
+    
+    [Inject]
+    private void Construct(IUserInput userInput, Player player)
+    {
+        _userInput = userInput;
+        _player = player;
+        
+        _userInput.OnPress += OnPress;
+    }
+    
+    private void OnEnable() =>
+        _tapText.text = START_TEXT;
+
+    private void OnPress()
+    {
+        _tapText.gameObject.SetActive(false);
+        _userInput.OnPress -= OnPress;
+        _player.OnLose += OnLose;
+    }
+
+    private void OnLose()
+    {
+        _tapText.text = RESTART_TEXT;
+        _tapText.gameObject.SetActive(true);
+        _userInput.OnPress += Restart;
+    }
+
+    private void Restart() =>
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+}
